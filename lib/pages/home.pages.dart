b345c30267ea1d3f111cbd7a2a0d9e7b3f5eca38
RicardoTejedorRestrepo <103795653+RicardoTejedorRestrepo@users.noreply.github.com>
2022-09-16 21:41:49 -0500

    Update home.pages.dart

--- lib/pages/home.pages.dart
+++ lib/pages/home.pages.dart
@@ -1,6 +1,6 @@
-import 'dart:ffi';
-import 'dart:html';
-import 'dart:ui';
+//import 'dart:ffi';
+//import 'dart:html';
+//import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:noticias/pages/detalle.pages.dart';
@@ -14,8 +14,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  double peso = 0.0;
-  int edad = 0;
+  double peso = 88.0;
+  int edad = 27;
+  double valueSlider= 50;
 
   @override
   Widget build(BuildContext context) {
@@ -68,10 +69,22 @@ class _HomePageState extends State<HomePage> {
             child: Padding(padding: const EdgeInsets.all(15),
                 child: Container(
                   child: Column(
+                    
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Estatura"),
-                      Text("166cm"),
+                        Text("Estatura", style: TextStyle(color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),),                        
+                        Slider(value: valueSlider,
+                              activeColor: Color.fromARGB(255, 14, 1, 66),
+                              inactiveColor: Color.fromARGB(255, 255, 255, 255),
+                              min: 50,
+                              max: 250,
+                              divisions: 200,
+                              label: valueSlider.round().toString(),
+                               onChanged: (double value) {
+                                   setState(() {
+                                     valueSlider = value;
+                                   });}),
+                        Text(valueSlider.round().toString() + " cm", style: TextStyle(color: Colors.black45, fontSize: 50, fontWeight: FontWeight.bold),),  
                     ],
                   ),
                   decoration: BoxDecoration(
@@ -160,7 +173,47 @@ class _HomePageState extends State<HomePage> {
           )),
           GestureDetector(
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: ((context) => DetallePage())));
+              double valorIMC = peso /((valueSlider/100) * (valueSlider/100));
+              String estado ="";
+              Color colorEstado = Colors.black;
+              String msj = "";
+
+              if(valorIMC < 18.5){
+                estado = "Bajo Peso";
+                colorEstado = Colors.orange;
+                msj = "Tu peso corporal con relacion a la estatura es baja";
+              }
+              if(valorIMC >= 18.5 && valorIMC <24.9){
+                estado = "Normal";
+                colorEstado = Colors.green;
+                msj = "Tu peso es Normal ¡Buen Trabajo!!";
+              }
+              if(valorIMC > 25 && valorIMC <29.9){
+                estado = "Sobrepeso";
+                colorEstado = Color.fromARGB(255, 255, 89, 0);
+                msj = "Tienes Sobrepeso, ten cuidado!! cuida tu ingesta calorica";
+              }
+              if(valorIMC > 30 && valorIMC < 34.9){
+                estado = "Obesidad 1";
+                colorEstado = Color.fromARGB(255, 255, 0, 0);
+                msj = "OJO!! estas en Obesidad grado 1, Cuidate";
+              }
+              if(valorIMC > 35 && valorIMC < 39.9){
+                estado = "Obesidad 2";
+                colorEstado = Color.fromARGB(255, 200, 0, 0);
+                msj = "Lo sentimos, el Mensaje esta en proceso";
+              }
+              if(valorIMC > 40  && valorIMC < 49.9){
+                estado = "Obesidad 3";
+                colorEstado = Color.fromARGB(255, 150, 0, 0);
+                msj = "Lo sentimos, el Mensaje esta en proceso";
+              }
+              if(valorIMC > 50){
+                estado = "Obesidad 4";
+                colorEstado = Color.fromARGB(255, 100, 0, 0);
+                msj = "Lo sentimos, el Mensaje esta en proceso";
+              }
+              Navigator.push(context, MaterialPageRoute(builder: ((context) => DetallePage(valorIMC: valorIMC, estado: estado, colorEstado: colorEstado, msj:msj))));
             },
                 child: Container(
                   height: 80,
